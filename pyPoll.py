be63b5a7162bfe3b10e5731253555f58b8d37c6d
Irma <irivera@phinational.org>
2022-10-23 00:50:38 -0400

    Adding the election audit code.

--- pyPoll.py
+++ pyPoll.py
@@ -15,6 +15,23 @@ file_to_load = os.path.join("Resources/election_results.csv")
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# 1. Initialize variable that will increment by 1 as we read each row in the for loop. Called an accumulator. Line 63 prompts this step.
+total_votes = 0
+
+# Declare new list for candidate options. Line 72 prompts this step.
+candidate_options = []
+
+# Declare new dictionary for candidate votes.
+candidate_votes = {}
+
+# Declare variable for empty string for winning candidate.
+# Declare a variable for the "winning count" equal to zero.
+# Declare a variable for the "winning_percentage" equal to zero.
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file
 with open(file_to_load) as election_data:
     
@@ -56,9 +73,81 @@ with open(file_to_load) as election_data:
 
     # Print the header row.
     headers = next(file_reader)
-    print(headers)
     
+    # Print each row in the CSV file.
+    for row in file_reader:
+    # Initialize accumulator variable above the with open() statement on line 22.
 
+      # 2. Add the total vote count.  
+        total_votes += 1
+      
+      # Print the candidate name from each row.
+        candidate_name = row[2]
+      
+      # Add the candidate_name to the candidate _options list using the append() method.
+        # candidate_options.append(candidate_name)
+
+      # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            
+            # Add it to the list of candidates. Add the candidate_name to the candidate _options list using the append() method.
+            candidate_options.append(candidate_name)
+
+            # Create each candidate as a key.
+            candidate_votes[candidate_name] = 0
+
+            # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
+
+# Print the candidate list.
+# print(candidate_options)
+
+# Print the candidate vote dictionary.
+# print(candidate_votes)
+    
+#3. Print the total votes
+# print(total_votes)
+
+# Declare a new list before the with open() statement to add candidate_options.
+# Declare a new dictionary at line 25 for candidate votes.
+    
+# Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    
+    # 2. Retreive vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    
+    # 3. Calculate the percentage of votes.
+    vote_percentage = float(votes) /float(total_votes) * 100
+
+    # 4. Print the candidate name and percentage of votes.
+    # print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
+
+  #  To do: print out each candidate's name, vote count, and percentage of
+  # votes to the terminal.
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+    
+  # Determine winning vote count and candidate
+  # 1. Determine if the votes are greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+      # 2. If true then set winning_count = votes and winning_percent =
+      # vote_percentage.
+      winning_count = votes
+      winning_percentage = vote_percentage
+       # 3. Set the winning_candidate equal to the candidate's name.
+      winning_candidate = candidate_name
+
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
+
+#  To do: print out the winning candidate, vote count and percentage to
+#   terminal.
 
        
 
