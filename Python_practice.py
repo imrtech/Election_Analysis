398e3bf24245ce2b463a6d7cb88dffa7aa5a492e
Irma <irivera@phinational.org>
2022-10-22 21:18:31 -0400

    pushing files.

--- Python_practice.py
+++ Python_practice.py
@@ -115,6 +115,37 @@
 # print(message_to_candidate)
 
 counties_dict = {"Arapahoe": 422829, "Denver":463353, "Jefferson": 432438}
-message_to_candidate = (
-    f"county" + "county has" {voters:,3f} + "registered voters"
-print(counties)
+for county in counties_dict:
+    print(county)  
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+{"county":"Denver", "registered_voters": 463353},
+{"county":"Jefferson", "registered_voters": 432438}]
+for county_dict in voting_data:
+    print(county_dict)
+
+    for county_dict in voting_data:
+        for value in county_dict.values():
+            print(value)
+
+# print each county and registered voter from the counties dictionary.
+    for county, voters in counties_dict.items():
+        print(county + " county has " + str(voters) + " registered voters.")
+
+# use f-stings to rewrite the print statement above to be more intuitive and clear
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters:,} registered voters.")
+    
+# Assign a variable for the file to load and the path.
+file_to_load = 'Resources/election_results.csv' 
+# Open election results and use 'r' to read the file.
+election_data = open(file_to_load, 'r')
+
+# To do: perform analysis.
+# Close the file.
+election_data.close()
+
+# Open the election results and read the file
+with open(file_to_load) as election_data:
+    # To do: perform analysis.
+     print(election_data)
